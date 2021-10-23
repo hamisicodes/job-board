455e13e6f92bed75bdadb29b752e96a726caa839
Hamisi Ngao <hamisi.n@code-care.pro>
2021-10-23 23:32:20 +0300

    configure dj-db-url

--- job_board/settings.py
+++ job_board/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +136,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)

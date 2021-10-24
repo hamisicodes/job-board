088306b571ba2b26bf248288026ee76683d69073
Hamisi Ngao <hamisi.n@code-care.pro>
2021-10-24 12:49:30 +0300

    CRUD for comments

--- board/urls.py
+++ board/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PostsView, PostView
+from .views import PostsView, PostView, CommentsView, CommentView
 
 urlpatterns = [
     path('news-list/', PostsView.as_view(), name='news_list'),
     path('<pk>/', PostView.as_view(), name='news_detail'),
+    path('news-comments', CommentsView.as_view(), name='news_comment'),
+    path('comments/<pk>', CommentView.as_view(), name='comment_details')
 ]

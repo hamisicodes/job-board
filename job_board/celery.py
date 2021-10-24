5cab59941ad472336346e37595713393d0e3bf78
Hamisi Ngao <hamisi.n@code-care.pro>
2021-10-24 21:31:50 +0300

    celery configs

--- job_board/celery.py
+++ job_board/celery.py
@@ -1,19 +1,23 @@
+from __future__ import absolute_import
+
 import os
+import django
 from celery import Celery
 from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'job_board.settings')
+django.setup()
 
 app = Celery('job_board',
-             broker='pyamqp://guest@localhost//',
-             backend='rpc://',
+             broker=os.environ.get('CLOUDAMQP_URL'),
              include=['job_board.tasks'])
 
 app.conf.beat_schedule = {
     'reset-post-upvotes-count': {
         'task': "job_board.tasks.reset_upvotes",
-        'schedule': crontab(hour='24',
-                            minute=0,
-                            )
+        'schedule': crontab(hour='23', minute=0)
     }
 }
+
+if __name__ == '__main__':
+    app.start()
